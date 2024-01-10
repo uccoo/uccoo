92ae524710b984478c3ff5aaca3862aec68b3754
uccoo <10.china@163.com>
2024-01-10 22:12:34 +0800

    seperate

---
+++ scripts/biomed.js
@@ -0,0 +1,19 @@
+/* biomed.js */
+
+function handleEnter(event) {
+    if (event.key === "Enter") {
+        searchBing();
+    }
+}
+
+function searchBing() {
+    window.open("https://www.bing.com/search?q=" + encodeURIComponent(document.querySelector('input').value), "_blank");
+}
+
+function searchGoogle() {
+    window.open("https://www.google.com/search?q=" + encodeURIComponent(document.querySelector('input').value), "_blank");
+}
+
+function searchBaidu() {
+    window.open("https://www.baidu.com/s?wd=" + encodeURIComponent(document.querySelector('input').value), "_blank");
+}
